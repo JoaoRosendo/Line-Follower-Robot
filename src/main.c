79382fd044f5063d4f1f392ba22461f0ce0d165c
JoaoRosendo <joao.rafael_7@hotmail.com>
2020-12-07 23:10:45 +0000

    lcd update

--- src/main.c
+++ src/main.c
@@ -4,7 +4,9 @@
 #include "LineSensor.h"
 #include "motor.h"
 #include "serial_printf.h"
+#include "lcd.h"
 
+uint8_t teste = 100;
 
 
 void pin_read()             
@@ -14,20 +16,44 @@ void pin_read()
 
 
 int main(void)
-{      
-    DDRB |= 0b00111111;
-    sei();
+{    
+   // DDRC |= (1 << PC5) | (1 << PC4);  
+   // PORTC |= (1 << PC5) | (1 << PC4);  
+    //sei();
     printf_init(); 
-    ADC_init();
-    motor_init(); //desligar para retornar funcionalidade aos pinos que usam PWM
+   // ADC_init();
+    //motor_init(); //desligar para retornar funcionalidade aos pinos que usam PWM
+    
+    
+    
+    
+    //i2c_init();
+    
+    start();
+
+   _delay_ms(1000);
+     clearScreen();
+   Send_A_String("tou    TOU ttttttttttt");
+   _delay_ms(1000);
+
+   clearScreen();
+
+   lcd_info_print("O numero e %d \n", teste);
     
 while (1) 
 {   
+   // lcd_print("TOU");
+   /* PORTB |= (1<< PB0) | (1<< PB4);
+    OCR1AL=255;
+    OCR1BL=255; */
+                                             //-     Display initialization
+        
+                                              //-     Move to position (line 2, row 4)
+        
+        //-     Turn cursor off and activate blinking
+        //lcd_command(LCD_DISPLAYON | LCD_CURSOROFF | LCD_BLINKINGON);
     
-    OCR1AL=150;
-  
-   
-    pin_read();
+
     //printf("IR1:%d  IR2:%d  IR3:%d  IR4:%d  IR5:%d \n",IR[0],IR[1],IR[2],IR[3],IR[4]); //valores do sensor de linha
 }       
 }
--- src/motor.c
+++ src/motor.c
@@ -10,12 +10,23 @@ ISR(TIMER1_COMPB_vect){
 
 
 void motor_init()
-{
+{   
+    //Inicialização dos portos do controlar do motor
+
+    DDRB|= (1<< PB0) //BIN1
+         | (1<< PB1) //PWMB
+         | (1<< PB2) //PWMA
+         | (1<< PB3) //BIN2
+         | (1<< PB4) //AIN1
+         | (1<< PB5);//AIN2
+
+    //Inicialização do timer gerador de PWM
+    
     TCCR1A |= (1 << COM1A1) | (1 << COM1B1) | (1 << WGM10);
     TCCR1B |= (1 << WGM12); //Escolha do modo (fast pwm 8 bit)
     TCCR1B |= (1 << CS12) | (1 << CS10); //Set timer prescaler 1024 (15625 Hz)<100kHz ( 100kHz -> maximo do driver do motor)
     TIMSK1 |= (1 << OCIE1A) | (1 << OCIE1B); //Liga interrupts
-    OCR1BL = 130;
+    
 }
 
 //fazer funcao que rtecebe parametros com velocidade do motor de 0 a 100, que modifica o pwm dos motores
